7b6f61240a7fad4704108adb65da086426bb6264
leejongcheal <aksndk123@naver.com>
2022-09-17 18:08:23 +0900

    ***** 처음에 접근도 못함
    **** 원래는 key값 변화생기면 도는걸로 했는데, 이건 lock부분을 가지고 있다가 key을 만나면 lock도 큐에 넣는식으로 한바퀴만 돌도록 변경함.
    그리고 효율성은 맞는데 그냥 dfs로 안풀어서 틀림 그냥 넘기자
    솔직히 n이 20만이라 쫄았음. 근데 예전에 풀었던 백준 열쇠문제랑 비슷하다고함
    그건 풀었는데 이건 못푸냐 ㅋㅋ
    ***** order부분에 0 이들어가는 경우를 체크해야함 ㅋㅋ
    --- 근데 이거 처리했는데 이상하게 안됨 파이썬 개병신
    쩄든 효율성은 개털림 --- 아아 key값이 0인 경우에는 key>1로 체크를 못함 이런 개같은 실수

--- 2020 인턴쉽/동굴 탐험 - LV 4.py
+++ 2020 인턴쉽/동굴 탐험 - LV 4.py
@@ -1,30 +1,34 @@
 from collections import defaultdict, deque
 
 
-def bfs(visit_key, graph, check, now_key, visit_n):
+def bfs(graph, lock, key, visited):
     q = deque()
     q.append(0)
-    visit_key[0] = len(now_key)
+    visited[0] = True
+    block_lock = []
     while q:
         x = q.popleft()
-        visit_n[x] = 1
+        visited[x] = True
         for next in graph[x]:
-            if check[next] == 1:
-                now_key.add(next)
-            elif check[next] > 1:
-                if check[next] not in now_key:
-                    continue
-            if visit_key[next] == len(now_key) and visit_n[next] == 1:
-                continue
-            visit_key[next] = len(now_key)
-            q.append(next)
-    return now_key
+            if not visited[next]:
+                if lock[next]:
+                    if not visited[lock[next]]:
+                        block_lock.append(next)
+                        visited[next] = True
+                        continue
+                if key[next]:
+                    if key[next] in block_lock:
+                        q.append(key[next])
+                        block_lock.remove(key[next])
+                visited[next] = True
+                q.append(next)
+
 
 def solution(n, path, order):
     graph = defaultdict(list)
-    visit_n = [0]*n
-    visit_key = defaultdict(int) # 각 정점의 최근 방문했을떄 key의 갯수 저장
-    check = defaultdict(int) # lock라면 해당 key숫자적어주기 // key라면 1적기.
+    visited = [False]*n
+    key = [0]*n
+    lock = [0]*n
     now_key = set() # 방문한 key 모음
     for a, b in path:
         graph[a].append(b)
@@ -34,15 +38,10 @@ def solution(n, path, order):
             continue
         elif b == 0:
             return False
-        check[a] = 1
-        check[b] = a
-
-    while 1:
-        prev_key = len(now_key)
-        bfs(visit_key, graph, check, now_key, visit_n)
-        if prev_key == len(now_key):
-            break
-    if sum(visit_n) == n:
+        lock[b] = a
+        key[a] = b
+    bfs(graph, lock, key, visited)
+    if sum(visited) == n:
         return True
     else:
         return False
