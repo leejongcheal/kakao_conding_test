683236a52bd0c33e38116c06691291c5f4b8d3be
leejongcheal <aksndk123@naver.com>
2022-09-17 16:16:51 +0900

    ***** 탐색문제 + 힙관련된 중복방문 처리 개념
    일단 코너부분 이해를잘못함 +600해주면됨
    최소라 BFS로 풀이할려고함.
    그리고 d == -1인데 nd == -1로 해줘서 처음에 가는거 실수함
    ++ 시간초과가 뜸 왜뜨는거지 ㅋㅋ visit이 에반가?
    -> visit은 상관없음..... 그냥 cost로만 비교하니깐 cost는 큰데 x,y를 또 방문하는 경우를 중복처리해줘야함.
    --> x,y,d에 따른 cost를 저장해서 풀이
    
    더풀어 쓰자면 힙도 그렇지만 BFS도 step이 상하좌우인 경우 돌아서 가버리는 괴상한 경우의 수가 있기때문에 효율을 위해서 처리해줘야함

--- 2020 인턴쉽/경주로 건설 - LV 3.py
+++ 2020 인턴쉽/경주로 건설 - LV 3.py
@@ -1,21 +1,26 @@
 from heapq import heappush, heappop
-
+from collections import defaultdict
 
 def solution(board):
-    answer = 0
     N = len(board)
+    check = defaultdict(int)
     steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     # 비용 ,x,y,d,visit
     visit = [(0, 0)]
     q = [(0, 0, 0, -1, visit)]
     while q:
         cost, x, y, d, visit = heappop(q)
+        if check[(x,y,d)] == 0:
+            check[(x,y,d)] = cost
+        else:
+            if cost > check[(x,y,d)]:
+                continue
         if x == N - 1 and y == N - 1:
             return cost
         for nd, (dx, dy) in enumerate(steps):
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0 and (nx, ny) not in visit:
-                if d == -1 or nd % 2 == d % 2: # nd == -1로 놓아서 실수함
+                if d == -1 or nd == d: # nd == -1로 놓아서 실수함
                     heappush(q, (cost + 100, nx, ny, nd, visit + [(nx, ny)]))
                 else:
                     heappush(q, (cost + 600, nx, ny, nd, visit + [(nx, ny)]))
